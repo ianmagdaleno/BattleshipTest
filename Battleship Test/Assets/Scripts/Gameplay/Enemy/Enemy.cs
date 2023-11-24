8f1375455258b45610a78a95d3e27f4f30810883
ianmagdaleno <ianmagdaleno1726@gmail.com>
2023-11-24 17:58:56 -0300

    Implemented movement using pathfinding at enemy class
    -Implemented pathfinding at enemy class

--- Battleship Test/Assets/Scripts/Gameplay/Enemy/Enemy.cs
+++ Battleship Test/Assets/Scripts/Gameplay/Enemy/Enemy.cs
@@ -4,52 +4,107 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Enemy basic stats")]
     [Range(1, 20)]
     [SerializeField] private float speed;
 
     [Range(1, 20)]
     [SerializeField] private int damage;
 
+    [Range(0.3f, 5f)]
+    [SerializeField] private float rangeToAct;
     [SerializeField] private float maxHealth;
-    [SerializeField] private HealthShipManager healthShipManager;
-    [SerializeField] private Transform targetPlayer;
-    [SerializeField] private Rigidbody2D localRigidbody;
-    [SerializeField] private LayerMask obstacleLayer;
 
-    protected void Start()
+    [Space(10), Header("Enemy basic Components")]
+    [SerializeField] private HealthShipManager healthShipManager;
+    [SerializeField] private Rigidbody2D localRigidbody;
+    [SerializeField] Transform bodyTransform;
+
+    private Transform targetPlayer;
+    private Pathfinding pathfinding;
+    private int currentPathIndex;
+    private List<Vector3> pathVectorList;
+
+    public void Start()
     {
         if (targetPlayer == null)
         {
             targetPlayer = GameObject.FindGameObjectWithTag("Player").transform;
             healthShipManager.Initialize(DataManager.GetEnemyCurrentTeam());
         }
+
+        pathfinding = new Pathfinding(20, 12);
     }
 
-    protected void Update()
+    public void FixedUpdate()
     {
-        FollowTheTarget();
+        RotateAim();
+        HandleMovement();
+        SetTargetPosition(targetPlayer.position);
+        DrawPathToTarget(targetPlayer.position);
     }
 
-    public void FollowTheTarget()
+    public void RotateAim()
     {
         if (targetPlayer != null)
         {
             Vector2 moveDirection = (targetPlayer.position - transform.position).normalized;
-
-          
-            //RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDirection, 2f, obstacleLayer);
-            //Debug.DrawRay(transform.position, transform.up * 2f, Color.green);
-
-            //if (hit.collider != null)
-            //{
-            //    Debug.Log("Hit it");
-            //    moveDirection = Quaternion.Euler(0, 0, Random.Range(90, 270)) * moveDirection;
-            //}
-
-            localRigidbody.velocity = moveDirection * speed;
-
             float aimAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90f;
             localRigidbody.rotation = aimAngle;
+        }
+    }
+    public void HandleMovement()
+    {
+        if (pathVectorList != null)
+        {
+            Vector3 targetPosition = pathVectorList[currentPathIndex];
+            if (Vector3.Distance(transform.position, targetPosition) > rangeToAct)
+            {
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+                transform.position = transform.position + moveDirection * speed * Time.deltaTime;
+            }
+            else
+            {
+                ShipAction();
+                currentPathIndex++;
+                if (currentPathIndex >= pathVectorList.Count)
+                {
+                    StopMoving();
+                }
+            }
+        }
+    }
+    public void ShipAction()
+    {
+        
+    }
+    public void StopMoving()
+    {
+        pathVectorList = null;
+    }
+    protected void SetTargetPosition(Vector3 targetPosition)
+    {
+        currentPathIndex = 0;
+        pathVectorList = pathfinding.FindPath(transform.position, targetPosition);
+
+        if (pathVectorList != null && pathVectorList.Count > 1)
+        {
+            pathVectorList.RemoveAt(0);
+        }
+    }
+    protected void DrawPathToTarget(Vector3 targetPosition)
+    {
+        pathfinding.GetGrid().GetXY(transform.position, out int startX, out int startY);
+        pathfinding.GetGrid().GetXY(targetPosition, out int endX, out int endY);
+
+        List<PathNode> path = pathfinding.FindPath(startX, startY, endX, endY);
+
+        if (path != null)
+        {
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                Debug.DrawLine(new Vector3(path[i].x, path[i].y) + Vector3.one * .5f, new Vector3(path[i + 1].x, path[i + 1].y) + Vector3.one * .5f, Color.red);
+            }
         }
     }
 }
--- Battleship Test/Assets/Scripts/Gameplay/Enemy/EnemyShooter.cs
+++ Battleship Test/Assets/Scripts/Gameplay/Enemy/EnemyShooter.cs
@@ -7,16 +7,6 @@ public class EnemyShooter : Enemy
     [SerializeField] private float rangeToShoot;
     [SerializeField] private Weapon weapon;
 
-    private float distanceAtPlayer;
-    private void  FixedUpdate()
-    {
-        //if(distanceAtPlayer > rangeToShoot)
-        //{
-            FollowTheTarget();
-        //}
-        //else 
-        //{
-        //    weapon.Shoot();
-        //}
-    }
+    
+    
 }
